8bd81630669726c3975a4c339d29d67067ad7196
Stephen Hendrikse <stehendrikse@hotmail.co.uk>
2021-03-04 17:28:01 +0000

    Basic Bundle class 'frame' created

---
+++ src/components/Bundle.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+class Bundle {
+  constructor(type, id, objects) {
+	  this.type = type;
+	  this.id = id;
+	  this.objects = objects;
+  }
+  setDetails() {}
+  addObject(){}
+  modifyObject(){}
+  deleteObject(){}
+  generateJSON(){}
+}
+
+let test = {
+	let this.type = "bundle";
+	let this.id = "bundle--5d0092c5-5f74-4287-9642-33f4c354e56d";
+	
+	return type,
+}
+
+
+
+export default Bundle;
